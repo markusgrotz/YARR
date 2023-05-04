1fe9a4e48d45830b1ab9708f8957762ee35b5c12
Markus Grotz <grotz@uw.edu>
2023-05-04 15:53:00 -0700

    add BimanualAgent

--- yarr/agents/agent.py
+++ yarr/agents/agent.py
@@ -76,3 +76,70 @@ class Agent(ABC):
     @abstractmethod
     def save_weights(self, savedir: str) -> None:
         pass
+
+
+class BimanualAgent(Agent):
+    """
+    
+    """
+
+    def __init__(self, right_agent: Agent, left_agent: Agent):
+        self.right_agent = right_agent
+        self.left_agent = left_agent
+
+    def build(self, training: bool, device=None) -> None:
+        self.right_agent.build(training, device)
+        self.left_agent.build(training, device)
+
+    def update(self, step: int, replay_sample: dict) -> dict:
+        raise Exception("not implemented")
+
+    def act(self, step: int, observation: dict, deterministic: bool) -> ActResult:
+
+        observation_elements = {}
+        info = {}
+
+        right_observation = {}
+        left_observation = {}
+
+        for k, v in observation.items(): 
+            if "rgb" in k or "point_cloud" in k or "camera" in k:
+                right_observation[k] = v
+                left_observation[k] = v
+            elif "right_" in k :
+                right_observation[k[6:]] = v
+            elif "left_" in k:
+                left_observation[k[5:]] = v
+            else:
+                right_observation[k] = v
+                left_observation[k] = v
+
+        right_act_result = self.right_agent.act(step, right_observation, deterministic)
+        left_act_result = self.left_agent.act(step, left_observation, deterministic)
+
+        action = (*right_act_result.action, *left_act_result.action)
+
+        observation_elements.update(right_act_result.observation_elements)
+        observation_elements.update(left_act_result.observation_elements)
+
+        info.update(right_act_result.info)
+        info.update(left_act_result.info)
+
+        return ActResult(action, observation_elements=observation_elements, info=info)
+
+    def reset(self) -> None:
+        self.right_agent.reset()
+        self.left_agent.reset()
+
+    def update_summaries(self) -> List[Summary]:
+        return self.right_agent.update_summaries() + self.left_agent.update_summaries()
+
+    def act_summaries(self) -> List[Summary]:
+        return self.right_agent.act_summaries() + self.left_agent.act_summaries()
+    
+    def load_weights(self, savedir: str) -> None:
+        self.right_agent.load_weights(savedir.replace("%ROBOT_NAME%", "right"))
+        self.left_agent.load_weights(savedir.replace("%ROBOT_NAME%", "left"))
+
+    def save_weights(self, savedir: str) -> None:
+        raise Exception("not implemented")
